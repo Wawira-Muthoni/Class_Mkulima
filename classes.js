286c41a574adce2d6f7297de10f865940ba80b59
Wawira-Muthoni <christinewawiramuthoni@gmail.com>
2022-07-23 15:07:49 +0300

    first commit

---
+++ classes.js
@@ -0,0 +1,60 @@
+
+class Mkulima{
+    constructor(){
+    this.farms = [];
+    this.groceryVendor = [];
+    this.product = [];
+    this.addFarm = (farmId, farmName, farmer, phone, address) => {
+    this.farms.push({farmId,farmName,farmer,phone,address})
+    }
+    this.removeFarm = (farmId) => {
+    let specific = this.farms.find(item => item.farmId ===farmId);
+    let farmIndex = this.farms.indexOf(specific);
+    this.farms.splice(farmIndex, 1);
+    }
+    this.updateFarm = function(farmId,newFarmId,newFarmName,newFarmerName,newPhone,newAddress){
+    let specificFarm = this.farms.find(item => item.farmId ===farmId)
+    specificFarm.farmId =newFarmId;
+    specificFarm.farmName =newFarmName;
+    specificFarm.farmer =newFarmerName;
+    specificFarm.phone =newPhone;
+    specificFarm.address =newAddress;
+    }
+    this.getFarm = (Id)=>{
+    console.log(this.farms.find(object=>object.farmId===Id))
+    }
+    this.addProduct = (productId, productName, price) => {
+    this.product.push({productId,productName,price})
+    
+    }
+    this.removeProduct = (productId) => {
+    let specific = this.product.find(item => item.productId ===productId);
+    let productIndex = this.product.indexOf(specific);
+    this.product.splice(productIndex,1);
+    }
+    this.updateProduct = function(productId,newProductId,newProductName,newPrice){
+    let specificProduct = this.product.find(item => item.productId ===productId)
+    specificProduct.productId =newProductId;
+    specificProduct.productName = newProductName;
+    specificProduct.price = newPrice;
+    }
+    this.getProduct = (Id)=>{
+    console.log(this.product.find(object=>object.productId===Id))
+    }
+    this.printProducts = ()=>{
+    console.log(this.product)
+    }
+    this.calculateOrderCost = (productId,quantity)=>{
+    let specificProduct = this.product.find(object=>object.productId===productId);
+    console.log(`${quantity} ${specificProduct.productName} for KES ${quantity*specificProduct.price}`);
+    }
+    }
+    }
+    let farm1 = new Mkulima();
+    farm1.addFarm("987","Icula","Mkulima","0798765433", "TK 543 T");
+    farm1.addFarm("001","Isaka","Mugunda","0712345678", "PK 987 P");
+    farm1.addFarm("105","Ilimi","Murimi","0756789210", "QW 003 Y");
+    console.log(farm1.farms);
+    
+    farm1.removeFarm("001");
+    console.log()
